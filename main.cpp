f1aa3c9aeba8d49c89813bce147630bc711884c0
qaw12lkj <118819820+qaw12lkj@users.noreply.github.com>
2024-11-17 15:54:42 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include ".assignment/test.h"
 
+struct Result
+{
+    int min;
+    int mid;
+    int max;
+};
+
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
@@ -23,5 +30,7 @@ void CompareThreeNumbers(int a, int b, int c, Result &result)
     c=b;
     b=t;
     }
-    
+    result.min = 最小值;
+    result.mid = 中间值;
+    result.max = 最大值;
 }
