8df867974f7a2b2e6f69dc8e53b8beccfec18cfe
qaw12lkj <118819820+qaw12lkj@users.noreply.github.com>
2024-11-17 15:52:46 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,4 +4,24 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    int t=0;
+    if(b>=a)
+    {
+    t=b;
+    b=a;
+    a=t;
+    }
+    if(c>=a)
+    {
+    t=c;
+    c=a;
+    a=t;
+    }
+    if(c>=b)
+    {
+    t=c;
+    c=b;
+    b=t;
+    }
+    
 }
